76172bfaf6b9ac0fa1d58476a92fbcfabc0ad046
Toffazal <muhammad.toffazal00@gmail.com>
2025-08-20 23:29:54 +0700

    perbaiki bug database

--- database/database.sql
+++ database/database.sql
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `galeri` (
   `keygaleri` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
